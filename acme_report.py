910e2dff8d4cfe9b86682b3c5800dfc3aa7dcebd
veritaem <35719608+veritaem@users.noreply.github.com>
2019-02-15 13:54:07 -0500

    Add files via upload

--- acme_report.py
+++ acme_report.py
@@ -5,7 +5,7 @@ from acme import *
 list1=['Awesome','Shiny','Portable', 'Improved', 'Impressive']
 list2=['Anvil', 'Catapult' 'Disguise' 'Mousetrap', '???']
 
-names list =[zip(x,list2) for x in itertools.permutations(list1,len(list2))]
+names_list =list(itertools.product(list1, list2))
 rando_int = random.randint(5, 101)
 rando_fire = random.randint(0.0, 2.6)
 
